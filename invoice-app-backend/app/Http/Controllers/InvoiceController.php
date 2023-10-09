7d9e613dcdff424d7e0f289c7f00477dcee4e4fb
Med Faji <meedfaji@gmail.com>
2023-10-09 17:39:06 +0100

    adding new route

--- invoice-app-backend/app/Http/Controllers/InvoiceController.php
+++ invoice-app-backend/app/Http/Controllers/InvoiceController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use Illuminate\Http\Request;
+use Exception;
 
 class InvoiceController extends Controller
 {
-    //
+    public function index() {
+        try {
+            $invoices = Invoice::with('customer')->orderBy('id', 'DESC')->get();
+            
+            return response()->json([
+                'invoices' => $invoices
+            ], 200);
+
+        }catch (Exception $e) {
+            response()->json($e);
+        }
+
+    }
 }
